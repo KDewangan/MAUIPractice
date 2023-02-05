b7d4e91be7cd1e5d72e37ae0b888e0c66b079364
khagesh dewangan <dewangankhagesh121@gmail.com>
2023-02-05 22:14:48 +0530

    Add files via upload

---
+++ FreshMAUIApp/FreshMAUIApp/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿namespace FreshMAUIApp;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+        this.BindingContext = new MainPageContext();
+    }
+  
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+        Console.WriteLine("Called OnAppearing;height:{0},width:{1}",this.Height,this.Width);
+    }
+    protected override void OnDisappearing()
+    {
+        base.OnDisappearing();
+        Console.WriteLine("Called OnDisappearing; height:{0},width:{1}", this.Height, this.Width);
+    }
+
+    protected override void OnSizeAllocated(double width, double height)
+    {
+        Console.WriteLine("Called OnSizeAllocated; height:{0},width:{1}", this.Height, this.Width);
+        base.OnSizeAllocated(width, height);
+    }
+
+    private void OnCounterClicked(object sender, EventArgs e)
+	{
+        Console.WriteLine("Called OnCounterClicked; height:{0},width:{1}", this.Height, this.Width);
+        count++;
+
+		if (count == 1)
+			CounterBtn.Text = $"Clicked {count} time";
+		else
+			CounterBtn.Text = $"Clicked {count} times";
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+}
+
+public class MainPageContext
+{
+    public string imgSource { get; set; } = "dotnet_bot.svg";
+    public bool IsEnabledProp { get; set; } = true;
+}
+
+
---
+++ FreshMAUIApp/FreshMAUIApp/Platforms/Android/PushNotificationListenerService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using Android.App;
+using Firebase.Messaging;
+
+
+[assembly: UsesPermission("com.google.android.c2dm.permission.RECEIVE")]
+namespace FreshMAUIApp.Platforms.Android
+{
+    [ExcludeFromCodeCoverage]
+    [Service(Exported = false)]
+    [IntentFilter(new[] { "com.google.firebase.MESSAGING_EVENT" }, Categories = new[] { "com.medtronic.mycarelinkheart.debug" })]
+    public class PushNotificationListenerService : FirebaseMessagingService
+    {
+		public PushNotificationListenerService()
+		{
+		}
+	}
+}
+
