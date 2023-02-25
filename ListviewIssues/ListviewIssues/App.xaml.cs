5b9a97b98a07841107287ac1d3511ec214a50ae8
khagesh dewangan <dewangankhagesh121@gmail.com>
2023-02-25 20:11:25 +0530

    Added new Project for MAUI Listview Issue practice
    Added new Project for MAUI Listview Issue practice

---
+++ ListviewIssues/ListviewIssues/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ListviewIssues;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ ListviewIssues/ListviewIssues/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ListviewIssues;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
